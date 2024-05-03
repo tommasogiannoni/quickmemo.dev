d7310803868b26661dfb416863989ab9066bb2f0
tommasogiannoni <tommaso.giannoni@yahoo.it>
2024-05-03 13:32:02 +0200

    first commit with page scheleton and some styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { CiCirclePlus } from "react-icons/ci";
 import './App.css';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const navigateToNewNote = () => {
+
+  }
+
+  const theme = {
+    backgroundColor: darkMode ? '#2d2d2d' : '#eeeeee',
+    color: darkMode ? '#eeeeee' : '#000000',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={theme}>
+      <div className="content">
+        <h1 className="title">Write.<span className='me'>me</span></h1>
+        <div>
+          <CiCirclePlus className="new-note-button" onClick={navigateToNewNote()}/>
+        </div>
+      </div>
+      <footer>
+        <nav>
+          <ul>
+            <li><a href="#about">About me</a></li>
+            <li className="change-theme" onClick={toggleDarkMode}><a href>{darkMode ? 'Light Mode' : 'Dark Mode'}</a></li>
+          </ul>
+        </nav>
+      </footer>
     </div>
   );
 }
