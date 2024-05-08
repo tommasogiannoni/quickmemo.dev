8fefae569c304f00ec538fa97b552bdcb25d1f67
tommasogiannoni <tommaso.giannoni@yahoo.it>
2024-05-08 18:18:45 +0200

    added height of title and content and manipulated it to show correctly the notes info

--- src/components/NotePage.js
+++ src/components/NotePage.js
@@ -1,17 +1,17 @@
-import {React, useEffect, useState,useRef } from 'react';
+import { React, useEffect, useState, useRef } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useLocation } from 'react-router-dom';
 import { Save } from 'lucide-react';
 import '../styles/NotePage.css'
 
-function NotePage({ theme, savedNotesList, setSavedNotesList })  {
+function NotePage({ theme, savedNotesList, setSavedNotesList }) {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [isClicked, setIsClicked] = useState(false);
   const [isSaved, setIsSaved] = useState(false);
-  const location = useLocation();
   const titleTextareaRef = useRef(null);
   const contentTextareaRef = useRef(null);
+  const location = useLocation();
 
   useEffect(() => {
     // edit mode note data
@@ -24,21 +24,15 @@ function NotePage({ theme, savedNotesList, setSavedNotesList })  {
         setContent(parsedNote[2]);
       }
     }
-    // Ripristina i valori del localStorage se esistono
-    const savedTextareaHeight = localStorage.getItem('textareaHeight');
-    if (savedTextareaHeight && titleTextareaRef.current) {
-      titleTextareaRef.current.style.height = savedTextareaHeight;
-      contentTextareaRef.current.style.height = savedTextareaHeight;
-    }
+  }, [location]);
 
-    // Aggiorna l'altezza della textarea solo se esiste un riferimento valido
+  useEffect(() => {
+    // Update textarea height after title or content change
     if (titleTextareaRef.current && contentTextareaRef.current) {
       updateTextareaHeight(titleTextareaRef.current);
       updateTextareaHeight(contentTextareaRef.current);
     }
-
-
-  }, [location]);
+  }, [title, content]);
 
   const updateTextareaHeight = (element) => {
     element.style.height = 'auto'; // Resetting the height to auto to calculate the scrollHeight
@@ -50,7 +44,7 @@ function NotePage({ theme, savedNotesList, setSavedNotesList })  {
       setTitle(event.target.value);
     }
   };
-  
+
   const handleContentChange = (event) => {
     if (event.target.value.length <= 5000) {
       setContent(event.target.value);
@@ -62,26 +56,35 @@ function NotePage({ theme, savedNotesList, setSavedNotesList })  {
     setTimeout(() => {
       setIsSaved(false);
     }, 2000);
-  }
+  };
 
   const clickOnSaveEffect = () => {
     setIsClicked(true);
     setTimeout(() => {
       setIsClicked(false);
     }, 400);
-  }
+  };
 
   const saveNotes = () => {
     // show effect
     clickOnSaveEffect();
+
+    // Aggiorna l'altezza delle textarea
+    if (titleTextareaRef.current && contentTextareaRef.current) {
+      updateTextareaHeight(titleTextareaRef.current);
+      updateTextareaHeight(contentTextareaRef.current);
+    }
+
     // edit note
-    if(location && location.state && location.state.note) {
-      if(savedNotesList && savedNotesList.length > 0) {
+    if (location && location.state && location.state.note) {
+      if (savedNotesList && savedNotesList.length > 0) {
         const found = savedNotesList.find(elem => elem[0] === location.state.note[0]);
-        if(found) {
-          const idx = savedNotesList.findIndex( elem => elem[0] === location.state.note[0] );
+        if (found) {
+          const idx = savedNotesList.findIndex(elem => elem[0] === location.state.note[0]);
           savedNotesList[idx][1] = title;
           savedNotesList[idx][2] = content;
+          savedNotesList[idx][3] = titleTextareaRef.current.scrollHeight;
+          savedNotesList[idx][4] = contentTextareaRef.current.scrollHeight;
           localStorage.setItem(savedNotesList[idx][0], JSON.stringify(savedNotesList[idx]));
         }
         savedEffectAlert();
@@ -90,47 +93,46 @@ function NotePage({ theme, savedNotesList, setSavedNotesList })  {
     }
 
     // new note
-    const newNote = ['note_' + uuidv4(), title, content, new Date().toLocaleDateString()];
-    if(title.length > 0 && content.length>0) {
+    const newNote = ['note_' + uuidv4(), title, content, titleTextareaRef.current.scrollHeight, contentTextareaRef.current.scrollHeight, new Date().toLocaleDateString()];
+    if (title.length > 0 && content.length > 0) {
       const newNotesList = [...savedNotesList, newNote];
       setSavedNotesList(newNotesList);
       localStorage.setItem(newNote[0], JSON.stringify(newNote));
     }
     savedEffectAlert();
-  }
-
+  };
 
   return (
     <div className="container">
       <div className='note-card'>
         <div className='note-actions'>
-          <button disabled={title.length === 0 || content.length === 0} 
-            className={`save-button ${isClicked ? 'clicked' : ''}`} 
-              style={{ color: theme.color }} 
-              onClick={saveNotes}>
-            <Save className='save-icon'/>
+          <button disabled={title.length === 0 || content.length === 0}
+            className={`save-button ${isClicked ? 'clicked' : ''}`}
+            style={{ color: theme.color }}
+            onClick={saveNotes}>
+            <Save className='save-icon' />
           </button>
         </div>
         {isSaved && <div className="alert">Saved!</div>}
         <div>
           <div className='input-field'>
-            <textarea 
-              type="text" 
-              className="note-title-input" 
-              placeholder="Title" 
-              value={title} 
-              onChange={handleTitleChange} 
-              ref={(textarea) => { if (textarea) updateTextareaHeight(textarea); }}
+            <textarea
+              type="text"
+              className="note-title-input"
+              placeholder="Title"
+              value={title}
+              onChange={handleTitleChange}
+              ref={titleTextareaRef}
             />
           </div>
           <div className='textarea-field'>
-            <textarea 
-              className="note-content-input" 
-              placeholder="Write me" 
-              value={content} 
+            <textarea
+              className="note-content-input"
+              placeholder="Write me"
+              value={content}
               onChange={handleContentChange}
-              style={{ color: theme.color}}
-              ref={(textarea) => { if (textarea) updateTextareaHeight(textarea); }} // Call updateTextareaHeight initially and whenever content changes
+              style={{ color: theme.color }}
+              ref={contentTextareaRef}
             />
           </div>
         </div>
