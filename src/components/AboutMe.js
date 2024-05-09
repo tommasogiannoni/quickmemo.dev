7b1ffa2ebeb67ba12b7020513c8e40378471ad1e
tommasogiannoni <tommaso.giannoni@yahoo.it>
2024-05-09 15:36:04 +0200

    updated about-me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,10 +9,10 @@ function AboutMe({theme}) {
         <div className="content-about">
             <div className='text-paragraph' style={{ color: theme.color }}>
             <div className='about-section'>
-                <p>👋 Hi there! I'm Tommaso, a front-end software engineer. I've developed this web application, an open-source project that I've showcased in my <a href="https://github.com/tommasogiannoni" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
-                <p>💡 I got ideas from different online apps to make a simple version where you can quickly write down notes and ideas straight from your browser. It works on mobile too, but I know it could be made even better. 📱</p>
+                <p>👋 Hi there! my name is Tommaso. I'm a front-end software engineer. This web application is an open-source project that i've showcased in my <a href="https://github.com/tommasogiannoni" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
+                <p>💡 I got ideas from different online apps to make a simple version where you can quickly write down notes and ideas straight from your browser. It works on mobile too.</p>
                 <p>🎨 I've put significant effort into both the architectural structure and design of the app. I'm always open to feedback or ideas to further enhance it!</p>
-                <p>🐞 If you encounter any bugs or have any ideas to make it even better, feel free to reach out to me via <a href="mailto:tommaso.giannoni@yahoo.it" target="_blank" rel="noopener noreferrer">email</a>. If you're a developer and want to contribute, go ahead and open a pull request on the GitHub project. I'm always open to new contributors!</p>
+                <p>🐞 If you're a developer and you encounter any bugs or have any ideas to make it even better, feel free to reach out to me via <a href="mailto:tommaso.giannoni@yahoo.it" target="_blank" rel="noopener noreferrer">email</a> or open a pull-request. I'm always open to new contributors!</p>
             </div>
             </div>
             <div className="author-links">
