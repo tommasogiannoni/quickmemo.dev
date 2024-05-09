3cece70696f3a7543cf6f4ca24630aec293371bf
tommasogiannoni <tommaso.giannoni@yahoo.it>
2024-05-09 20:46:50 +0200

    enanched style in about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,10 +9,10 @@ function AboutMe({theme}) {
         <div className="content-about">
             <div className='text-paragraph' style={{ color: theme.color }}>
             <div className='about-section'>
-                <p>👋 Hi there! my name is Tommaso. I'm a front-end software engineer. This web application is an open-source project that i've showcased in my <a href="https://github.com/tommasogiannoni" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
-                <p>💡 I got ideas from different online apps to make a simple version where you can quickly write down notes and ideas straight from your browser. It works on mobile too.</p>
-                <p>🎨 I've put significant effort into both the architectural structure and design of the app. I'm always open to feedback or ideas to further enhance it!</p>
-                <p>🐞 If you're a developer and you encounter any bugs or have any ideas to make it even better, feel free to reach out to me via <a href="mailto:tommaso.giannoni@yahoo.it" target="_blank" rel="noopener noreferrer">email</a> or open a pull-request. I'm always open to new contributors!</p>
+                <p>👋 Hi there! my name is <b style={{ fontWeight: '700' }}>Tommaso</b>. I'm a front-end software engineer. This web application is an <b style={{ fontWeight: '600'}}>open-source</b> project that i've showcased in my <a href="https://github.com/tommasogiannoni" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
+                <p>💡 I got ideas from different online apps to make a simple version where you can quickly write down notes and ideas straight from your browser. It works on <b style={{ fontWeight: '500' }}>mobile </b> too.</p>
+                <p>🎨 I've put significant effort into both the architectural structure and design of the app. I'm always <b style={{ fontWeight: '650', fontStyle: 'italic' }}>open to feedback or ideas</b> to further enhance it!</p>
+                <p>🐞 If you're a developer and you encounter any bugs or have any ideas to make it even better, feel free to reach out to me via <a href="mailto:tommaso.giannoni@yahoo.it" style={{ fontWeight: '650' }} target="_blank" rel="noopener noreferrer">email</a> or open a pull-request. I'm always open to new contributors!</p>
             </div>
             </div>
             <div className="author-links">
